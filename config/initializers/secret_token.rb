cae2e6d03a5839cc474b098bfc7da6ac1aee5576
zigomir <zigomir@gmail.com>
2012-10-13 14:49:29 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotCookies::Application.config.secret_token = 'b94bcd53f44a74e808a7191ebb7ef26f4228e35b37a320245e59e23ccf3fc3fa3fb0c6d9abfc0c842358fb7aa207d4981d9711812de59f226648712fc9023276'
